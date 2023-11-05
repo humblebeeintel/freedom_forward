7dc2726e8716daf1dc0708789cb465278ffb9495
habibullo-dev <habibullochutboev@gmail.com>
2023-11-05 16:31:53 +0900

    Some html elemetns updated

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ let palestineCountries = [
 
     "id": 1,
     "country": "Turkey",
-    "image_link": "",
+    "image_link": "https://www.al-monitor.com/sites/default/files/styles/article_header/public/almpics/2019/05/RTS19R85.jpg/RTS19R85.jpg?h=a5ae579a&itok=BbNvYeUV",
     "news": {
       "title": "Turkey voices support for Palestine",
       "brief": "Turkey has been a vocal supporter of Palestine and has condemned Israel's actions in the current conflict.",
@@ -165,7 +165,7 @@ let palestineCountries = [
     "reference": "https://www.aljazeera.com/news/2023/10/23/which-countries-have-sent-aid-to-gaza-so-far",
     "id": 2,
     "country": "Iran",
-    "image_link": "",
+    "image_link": "https://cdn.vox-cdn.com/thumbor/jlWKooXIp2FNHAKLs_tR3OXvcdY=/0x0:5472x3648/1200x800/filters:focal(2299x1387:3173x2261)/cdn.vox-cdn.com/uploads/chorus_image/image/72755984/1723107388.0.jpg",
     "news": {
       "title": "Iran voices support for Palestine",
       "brief": "Iran has been a vocal supporter of Palestine and has condemned Israel's actions in the current conflict.",
@@ -176,7 +176,7 @@ let palestineCountries = [
   {
     "id": 3,
     "country": "Qatar",
-    "image_link": "",
+    "image_link": "https://www.al-monitor.com/sites/default/files/styles/social_media_share/public/almpics/2018/12/rtx39tf1.jpg/rtx39tf1.jpg?h=a5ae579a&itok=6V1thqoL",
     "news": {
       "title": "Qatar voices support for Palestine",
       "brief": "Qatar has been a vocal supporter of Palestine and has condemned Israel's actions in the current conflict.",
@@ -187,7 +187,7 @@ let palestineCountries = [
   {
     "id": 4,
     "country": "Egypt",
-    "image_link": "",
+    "image_link": "https://eipss-eg.org/wp-content/uploads/2021/03/Egyptian-Palestinian-Relations-Since-the-2013-Coup.jpg",
     "news": {
       "title": "Egypt voices support for Palestine",
       "brief": "Egypt has been a vocal supporter of Palestine and has condemned Israel's actions in the current conflict.",
@@ -198,7 +198,7 @@ let palestineCountries = [
   {
     "id": 5,
     "country": "Pakistan",
-    "image_link": "",
+    "image_link": "https://cbx57v6vu4bo.wpcdn.shift8cdn.com/wp-content/uploads/2021/11/Palestine-to-take-trade-tourism-ties-with-Pakistan-to-optimum-level-Ambassador-1068x561.jpg",
     "news": {
       "title": "Pakistan voices support for Palestine",
       "brief": "Pakistan has been a vocal supporter of Palestine and has condemned Israel's actions in the current conflict.",
@@ -210,7 +210,7 @@ let palestineCountries = [
   {
     "id": 6,
     "country": "Turkey",
-    "image_link": "",
+    "image_link": "https://www.al-monitor.com/sites/default/files/styles/article_header/public/almpics/2019/05/RTS19R85.jpg/RTS19R85.jpg?h=a5ae579a&itok=BbNvYeUV",
     "news": {
       "title": "Turkey supports Palestine",
       "brief": "Turkey has been a vocal supporter of Palestine and has condemned Israel's actions in the region.",
@@ -219,42 +219,9 @@ let palestineCountries = [
     "reference": "https://www.livemint.com/news/world/israelhamas-war-canada-turkey-iran-egypt-qatar-here-is-the-list-of-nations-that-have-supported-palestine-gaza-11697363499130.html"
   },
   {
-    "id": 7,
-    "country": "Iran",
-    "image_link": "",
-    "news": {
-      "title": "Iran supports Palestine",
-      "brief": "Iran has expressed support for Palestine and has criticized Israel's actions in the region.",
-      "detailed": "Iran has expressed support for Palestine and has criticized Israel's actions in the region. Iranian leaders have called for an end to the violence and have condemned Israel's use of force against Palestinian civilians. Iran has also provided humanitarian aid to Gaza and has pledged to continue supporting Palestine."
-    },
-    "reference": "https://www.livemint.com/news/world/israelhamas-war-canada-turkey-iran-egypt-qatar-here-is-the-list-of-nations-that-have-supported-palestine-gaza-11697363499130.html"
-  },
-  {
-    "id": 8,
-    "country": "Egypt",
-    "image_link": "",
-    "news": {
-      "title": "Egypt supports Palestine",
-      "brief": "Egypt has expressed support for Palestine and has called for an end to the violence in the region.",
-      "detailed": "Egypt has expressed support for Palestine and has called for an end to the violence in the region. Egyptian President Abdel Fattah el-Sisi has condemned Israel's actions in Gaza and has called for a ceasefire. Egypt has also provided humanitarian aid to Gaza and has pledged to continue supporting Palestine."
-    },
-    "reference": "https://www.livemint.com/news/world/israelhamas-war-canada-turkey-iran-egypt-qatar-here-is-the-list-of-nations-that-have-supported-palestine-gaza-11697363499130.html"
-  },
-  {
-    "id": 9,
-    "country": "Qatar",
-    "image_link": "",
-    "news": {
-      "title": "Qatar supports Palestine",
-      "brief": "Qatar has expressed support for Palestine and has provided humanitarian aid to Gaza.",
-      "detailed": "Qatar has expressed support for Palestine and has provided humanitarian aid to Gaza. Qatar has also criticized Israel's actions in the region and has called for an end to the violence. Qatar has pledged to continue supporting Palestine."
-    },
-    "reference": "https://www.livemint.com/news/world/israelhamas-war-canada-turkey-iran-egypt-qatar-here-is-the-list-of-nations-that-have-supported-palestine-gaza-11697363499130.html"
-  },
-  {
     "id": 10,
     "country": "Sudan",
-    "image_link": "",
+    "image_link": "https://www.aljazeera.com/wp-content/uploads/2020/06/bb6cd4f3065d4e1ca6fc0eff195f87ef_18.jpeg?resize=770%2C513&quality=80",
     "news": {
       "title": "Sudan supports Palestine",
       "brief": "Sudan has expressed support for Palestine and has condemned Israel's actions in the region.",
@@ -265,7 +232,7 @@ let palestineCountries = [
   {
     "id": 11,
     "country": "Jordan",
-    "image_link": "",
+    "image_link": "https://static01.nyt.com/images/2021/05/14/world/14israel-gaza-briefing-jordan/merlin_187604757_f3c70313-b212-4a58-bfa6-95650a6807c2-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
     "news": {
       "title": "Jordan supports Palestine",
       "brief": "Jordan has expressed support for Palestine and has called for an end to the violence in the region.",
@@ -276,7 +243,7 @@ let palestineCountries = [
   {
     "id": 12,
     "country": "Canada",
-    "image_link": "",
+    "image_link": "https://www.newarab.com/sites/default/files/1232938622.jpeg",
     "news": {
       "title": "Canada supports Palestine",
       "brief": "Canada has expressed support for Palestine and has provided humanitarian aid to Gaza.",
@@ -289,16 +256,16 @@ let palestineCountries = [
 let israelBrands = [{
   "id": 1,
   "brand": "Apple",
-  "image_link": "",
+  "image_link": "https://static.timesofisrael.com/www/uploads/2016/08/000_FQ0UD.jpg",
   "news": {
       "title": "Apple to open new development center in Jerusalem",
       "brief": "Apple has announced plans to open a new development center in Jerusalem, which will focus on hardware and software development.",
       "detailed": "The center will employ hundreds of engineers and will be Apple's first development center in Israel. Apple CEO Tim Cook has said that the company is committed to supporting Israel's technology industry and sees the country as a key market for its products."
   },
-  "reference": "https://www.timesofisrael.com/apple-to-open-new-development-center-in-jerusalem/",
+  "reference": "https://www.timesofisrael.com/apple-to-open-new-development-center-in-jerusalem/"},{
   "id": 2,
   "brand": "Microsoft",
-  "image_link": "",
+  "image_link": "https://media.licdn.com/dms/image/D4D22AQHSAA4GblzpHA/feedshare-shrink_800/0/1679903878949?e=1701302400&v=beta&t=EbNHoxFzl7Z3A-R6bIvXZv2OAmdsNNqeofr6ebYsJIA",
   "news": {
       "title": "Microsoft to open first cloud data center in Israel",
       "brief": "Microsoft has announced plans to open its first cloud data center in Israel, which will provide cloud services to customers in the region.",
@@ -308,7 +275,7 @@ let israelBrands = [{
 },
 {       "id": 3,
   "brand": "Intel",
-  "image_link": "",
+  "image_link": "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iFq.iDmhOYho/v1/-1x-1.jpg",
   "news": {
       "title": "Intel to invest $10 billion in new Israeli chip plant",
       "brief": "Intel has announced plans to invest $10 billion in a new chip plant in Israel, which will produce advanced processors for use in data centers and other applications.",
@@ -318,7 +285,7 @@ let israelBrands = [{
 },{
   "id": 4,
   "brand": "Hyundai",
-  "image_link": "",
+  "image_link": "https://koreajoongangdaily.joins.com/data/photo/2023/10/17/885e9d62-4777-4da3-bdc7-459843561dbb.jpg",
   "news": {
       "title": "Amnesty International calls on Hyundai to end link with war crimes in Masafer Yatta",
       "brief": "Amnesty International has called on Hyundai to end its link with war crimes in Masafer Yatta, a Palestinian village in the West Bank.",
@@ -328,7 +295,7 @@ let israelBrands = [{
 },{
   "id": 5,
   "brand": "Puma",
-  "image_link": "",
+  "image_link": "https://www.amostrust.org/media/4063/amos_trust_palestine_justice_change_the_record_boycott_puma_carousel.jpg",
   "news": {
       "title": "Puma faces boycott over sponsorship of Israeli soccer teams",
       "brief": "Puma is facing a boycott over its sponsorship of Israeli soccer teams, which critics say is a violation of international law.",
@@ -338,7 +305,7 @@ let israelBrands = [{
 },{
   "id": 6,
   "brand": "Tom Ford",
-  "image_link": "",
+  "image_link": "https://media.licdn.com/dms/image/D4E0BAQH9tPiLt38cxA/company-logo_200_200/0/1695305566079?e=2147483647&v=beta&t=er2z4E0XEEsougatDEiiqIlaKhIJ4JnbpT2yTPKZqbM",
   "news": {
       "title": "Tom Ford supports Israel",
       "brief": "Tom Ford, the fashion designer, has expressed support for Israel.",
@@ -348,7 +315,7 @@ let israelBrands = [{
 },{
   "id": 7,
   "brand": "AXA XL",
-  "image_link": "",
+  "image_link": "https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fcom.ft.imagepublish.upp-prod-eu.s3.amazonaws.com%2Faea844bc-1f2b-11e8-a895-1ba1f72c2c11?source=next-article&fit=scale-down&quality=highest&width=700&dpr=1",
   "news": {
       "title": "AXA XL announces new leadership team in Israel",
       "brief": "AXA XL has announced a new leadership team in Israel, which will focus on expanding the company's presence in the region.",
@@ -358,7 +325,7 @@ let israelBrands = [{
 },{
   "id": 8,
   "brand": "Papa John's",
-  "image_link": "",
+  "image_link": "https://yeahthatskosher.com/wp-content/uploads/2021/08/papa-johns-pizza-kosher-israel-petach-tikva.jpg",
   "news": {
       "title": "Papa John's hires former Nike exec to learn from retailers",
       "brief": "Papa John's has hired a former Nike executive to help the company learn from retailers and improve its customer experience.",
@@ -370,7 +337,7 @@ let israelBrands = [{
 let israelCountries = [{
   "id": 1,
   "name": "United States", 
-  "image": "https://flagcdn.com/w320/us.png",
+  "image_link": "https://images.jpost.com/image/upload/f_auto,fl_lossy/c_fill,g_faces:center,h_428,w_640/430464",
   "news": {
     "title": "US Affirms Unwavering Support for Israel's Right to Defend Itself",
     "brief": "The US has expressed strong support for Israel's right to defend itself and has not condemned its military actions in Gaza.",
@@ -383,7 +350,7 @@ let israelCountries = [{
   ]},{
   "id": 2,
   "name": "United Kingdom", 
-  "image": "https://flagcdn.com/w320/gb.png",
+  "image_link": "https://static.jewishnews.co.uk/jewishnews/uploads/2020/04/Israeli-British-flag.jpg",
   "news": {
     "title": "UK Joins US and Other Countries in Supporting Israel",
     "brief": "The UK has joined the US and other countries in expressing support for Israel's right to defend itself.",
@@ -395,7 +362,7 @@ let israelCountries = [{
   ]},{
   "id": 3,
   "name": "France", 
-  "image": "https://flagcdn.com/w320/fr.png",
+  "image_link": "https://static.timesofisrael.com/www/uploads/2014/07/France-Pro-Israel-Pro_Horo1.jpg",
   "news": {
     "title": "France Joins US and Other Countries in Supporting Israel",
     "brief": "France has joined the US and other countries in expressing support for Israel's right to defend itself.",
@@ -407,7 +374,7 @@ let israelCountries = [{
   ]},{
   "id": 4,
   "name": "Australia", 
-  "image": "https://flagcdn.com/w320/au.png",
+  "image_link": "https://imengine.public.prod.mmg.navigacloud.com/?uuid=201027fe-53a5-597a-a690-08972a9044d9&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.75&x=0&y=0&width=1200&height=675",
   "news": {
     "title": "Australia Joins US and Other Countries in Supporting Israel",
     "brief": "Australia has joined the US and other countries in expressing support for Israel's right to defend itself.",
@@ -419,7 +386,7 @@ let israelCountries = [{
   ]},{
   "id": 5,
   "name": "Germany", 
-  "image": "https://flagcdn.com/w320/de.png",
+  "image_link": "https://cdn.prod.www.spiegel.de/images/1740cbab-4e6b-4e47-b163-defc49c9240b_w640_r1.3496_fpx37.03_fpy49.98.jpg",
   "news": {
     "title": "Germany Joins US and Other Countries in Supporting Israel",
     "brief": "Germany has joined the US and other countries in expressing support for Israel's right to defend itself.",
@@ -431,7 +398,7 @@ let israelCountries = [{
   ]},{
   "id": 6,
   "name": "Italy", 
-  "image": "https://flagcdn.com/w320/it.png",
+  "image_link": "https://www.israelhayom.com/wp-content/uploads/2021/02/israel-italy.jpg",
   "news": {
     "title": "Italy Joins US and Other Countries in Supporting Israel",
     "brief": "Italy has joined the US and other countries in expressing support for Israel's right to defend itself.",
@@ -443,7 +410,7 @@ let israelCountries = [{
   ]},{
   "id": 7,
   "name": "Canada", 
-  "image": "https://flagcdn.com/w320/ca.png",
+  "image_link": "https://thecjn.ca/wp-content/uploads/2015/11/Canada-Israel-Flag-e1561476924141.jpg",
   "news": {
     "title": "Canada Joins Other Western Nations in Supporting Israel",
     "brief": "Canada has joined other Western nations in condemning Hamas and expressing solidarity with Israel.",
@@ -478,15 +445,27 @@ function insertData(brandData, countryData, cardElements) {
 }
 
 function updateCards() {
+  // Select the h1 and p elements
+  const h1Element = document.querySelector('main .py-5 h1');
+  const pElement = document.querySelector('main .py-5 p');
+
   // Show/hide based on toggle
   if (toggle.checked) {
     showCards(palestineCards);
     hideCards(israelCards);
     insertData(palestineBrands, palestineCountries, palestineCards);
+
+    // Set the h1 and p content for the Palestine side
+    h1Element.textContent = 'Stand with Palestine';
+    pElement.textContent = 'Your source for supporting Palestine and advocating for justice.';
   } else {
     hideCards(palestineCards);
     showCards(israelCards);
     insertData(israelBrands, israelCountries, israelCards);
+
+    // Set the h1 and p content for the Israel side
+    h1Element.textContent = 'Boycott Israel: Nonviolent Activism for Palestinian Rights'; // replace with your actual text
+    pElement.textContent = 'Together we can promote activism, boycotts, and nonviolent resistance to achieve justice.'; // replace with your actual text
   }
 }
 
